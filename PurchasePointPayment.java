7d439c5c85ff0527cb3034d35e159513c1f76e69
5Kis2STRONG <c0a22114d7@edu.teu.ac.jp>
2023-12-11 20:13:42 +0900

    リファクタリングStep4 論理否定を戻し、コードを読みやすくする

--- PurchasePointPayment.java
+++ PurchasePointPayment.java
@@ -5,13 +5,12 @@ class PurchasePointPayment {
     final PurchasePoint consumptionPoint; // 購入で消費するポイント
     final LocalDateTime paymentDateTime; // 購入日時
     PurchasePointPayment(final Customer customer, final Comic comic) {
-        if (!customer.isEnabled()) {
+        if (customer.isDisabled()) {
             throw new IllegalArgumentException("有効な購入者ではありません。");
         }
-        if (!comic.isEnabled()) {
+        if (comic.isDisabled()) {
             throw new IllegalArgumentException("現在取り扱いのできないコミックです。");
-        }
-        if (customer.possessionPoint.amount < comic.currentPurchasePoint.amount) {
+        }   if (customer.possessionPoint.amount < comic.currentPurchasePoint.amount) {
             throw new RuntimeException("所持ポイントが不足しています。");
         }
         customerId = customer.id;
@@ -29,5 +28,8 @@ public class Customer {
     }
     public boolean isEnabled() {
         return true;
+    }
+    public boolean isDisabled() {
+        return false;
     }
 }
